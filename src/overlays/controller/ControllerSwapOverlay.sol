a5f8aaffb1c52803f6f9cf18e552cda034822262
fabiohild <fabiohildebrand@gmail.com>
2022-03-05 11:34:07 -0300

    WIP - controller swap

--- src/overlays/controller/ControllerSwapOverlay.sol
+++ src/overlays/controller/ControllerSwapOverlay.sol
@@ -8,14 +8,33 @@ import "geb-rrfm-calculators/calculator/PIScaledPerSecondCalculator.sol";
 abstract contract RateSetterLike {
     function modifyParameters(bytes32, address) external virtual;
     function updateRate(address) external virtual;
+    function pidCalculator() external virtual view returns (address);
 }
 
 abstract contract CalculatorLike {
     function modifyParameters(bytes32, address) external virtual;
+    function modifyParameters(bytes32, uint256) external virtual;
     function addAuthority(address) external virtual;
     function removeAuthority(address) external virtual;
+    function seedProposer() external virtual view returns (address);
+    function authorities(address) external virtual view returns (uint);
+    function sg() external virtual view returns (int256);
+    function ag() external virtual view returns (int256);
+    function pscl() external virtual view returns (uint256);
+    function ips() external virtual view returns (uint256);
+    function nb() external virtual view returns (uint256);
+    function foub() external virtual view returns (uint256);
+    function folb() external virtual view returns (int256);
+    function pdc() external virtual view returns (int256);
+    function lut() external virtual view returns (uint256);
+    function oll() external virtual view returns (uint256);
+    function dos(uint256) external virtual view returns (uint256, int256, int256);
+
 }
 
+abstract contract OracleRelayerLike {
+    function redemptionPrice() virtual external returns (uint256);
+}
 // @notice Swaps between raw and scaled controllers
 // @dev Needs to be authed in rateSetter
 contract ControllerSwapOverlay is GebAuth {
@@ -25,67 +44,114 @@ contract ControllerSwapOverlay is GebAuth {
     uint256 public lastUpdateTime;
     uint256 public immutable updateDelay;
     RateSetterLike public immutable rateSetter;
+    OracleRelayerLike public immutable oracleRelayer;
 
-    // raw calculator bounds
-    bytes32[] unsignedParams;
-    bytes32[] signedParams;
-    uint256[] unsignedUpperBounds;
-    uint256[] unsignedLowerBounds;
-    int256[] signedUpperBounds;
-    int256[] signedLowerBounds;
-
-    constructor(address _pauseProxy, RateSetterLike _rateSetter, uint256 _updateDelay) public GebAuth() {
-        pauseProxy = _pauseProxy;
-        rateSetter = _rateSetter;
-        updateDelay = _updateDelay;
+    struct OverlayBounds {
+        bytes32[] unsignedParams;
+        bytes32[] signedParams;
+        uint256[] unsignedUpperBounds;
+        uint256[] unsignedLowerBounds;
+        int256[] signedUpperBounds;
+        int256[] signedLowerBounds;
     }
 
-    function swapCalculator(
-            int256 Kp,
-            int256 Ki,
-            uint256 perSecondCumulativeLeak,
-            uint256 integralPeriodSize,
-            uint256 noiseBarrier,
-            uint256 feedbackOutputUpperBound,
-            int256  feedbackOutputLowerBound
-    ) external isAuthorized returns (address calculator, address overlay) {
-        require(lastUpdateTime + updateDelay < block.timestamp, "ControllerSwapOverlay/too-early");
+    OverlayBounds[] bounds;
+
+    constructor(address _pauseProxy, RateSetterLike _rateSetter, OracleRelayerLike _oracleRelayer, uint256 _updateDelay, bool _isScaled) public GebAuth() {
+        pauseProxy = _pauseProxy;
+        rateSetter = _rateSetter;
+        oracleRelayer = _oracleRelayer;
+        updateDelay = _updateDelay;
+        isScaled = _isScaled;
+
+
+        // populate bounds - index 0 == raw
+        bounds.push(OverlayBounds(
+            new bytes32[](0),
+            new bytes32[](0),
+            new uint256[](0),
+            new uint256[](0),
+            new int256[](0),
+            new int256[](0)
+        ));
+
+        // index 1 == scaled
+        bounds.push(OverlayBounds(
+            new bytes32[](0),
+            new bytes32[](0),
+            new uint256[](0),
+            new uint256[](0),
+            new int256[](0),
+            new int256[](0)
+        ));
+    }
+
+    int256 constant RAY = 10 ** 27;
+
+    function imul(int256 x, int256 y) internal pure returns (int256 z) {
+        require(y == 0 || (z = x * y) / y == x, "mul-overflow");
+    }
+
+    function swapCalculator() external isAuthorized returns (address calculator, address overlay) {
+        require(lastUpdateTime + updateDelay <= block.timestamp, "ControllerSwapOverlay/too-early");
+
+        CalculatorLike currentCalculator = CalculatorLike(rateSetter.pidCalculator());
+        uint256 redemptionPrice = oracleRelayer.redemptionPrice();
+
+        (
+            uint256 deviationTimestamp,
+            int256  deviationProportional,
+            int256  deviationIntegral
+        ) = currentCalculator.dos(currentCalculator.oll() - 1);
+
+        int256[] memory currentState = new int256[](5);
+        currentState[0] = int256(currentCalculator.lut());
+        currentState[1] = deviationProportional;
+        currentState[2] = deviationIntegral;
+        currentState[3] = currentCalculator.pdc();
+        currentState[4] = int256(deviationTimestamp);
+
         if (isScaled)
             calculator = address(new PIRawPerSecondCalculator(
-                Kp,
-                Ki,
-                perSecondCumulativeLeak,
-                integralPeriodSize,
-                noiseBarrier,
-                feedbackOutputUpperBound,
-                feedbackOutputLowerBound,
-                new int256[](0)
+                imul(currentCalculator.sg(), int256(redemptionPrice)) / RAY, // kp
+                imul(currentCalculator.ag(), int256(redemptionPrice)) / RAY, // ki
+                currentCalculator.pscl(),                                    // perSecondCumulativeLeak
+                currentCalculator.ips(),                                     // integralPeriodSize
+                currentCalculator.nb(),                                      // noiseBarrier
+                currentCalculator.foub(),                                    // feedbackOutputUpperBound
+                currentCalculator.folb(),                                    // feedbackOutputLowerBound
+                currentState
             ));
         else
             calculator = address(new PIScaledPerSecondCalculator(
-                Kp,
-                Ki,
-                perSecondCumulativeLeak,
-                integralPeriodSize,
-                noiseBarrier,
-                feedbackOutputUpperBound,
-                feedbackOutputLowerBound,
-                new int256[](5)
+                imul(currentCalculator.sg(), RAY) / int(redemptionPrice),    // kp
+                imul(currentCalculator.ag(), RAY) / int(redemptionPrice),    // ki
+                currentCalculator.pscl(),                                    // perSecondCumulativeLeak
+                currentCalculator.ips(),                                     // integralPeriodSize
+                currentCalculator.nb(),                                      // noiseBarrier
+                currentCalculator.foub(),                                    // feedbackOutputUpperBound
+                currentCalculator.folb(),                                    // feedbackOutputLowerBound
+                currentState
             ));
+
+        // set allReaderToggle
+        CalculatorLike(calculator).modifyParameters("allReaderToggle", 1);
 
         // swap controller con rate setter
         rateSetter.modifyParameters("pidCalculator", calculator);
         CalculatorLike(calculator).modifyParameters("seedProposer", address(rateSetter));
 
+        uint256 boundsIndex = (isScaled) ? 0 : 1;
+
         // overlay
         overlay = address(new MinimalRrfmCalculatorOverlay(
             calculator,
-            unsignedParams,
-            signedParams,
-            unsignedUpperBounds,
-            unsignedLowerBounds,
-            signedUpperBounds,
-            signedLowerBounds
+            bounds[boundsIndex].unsignedParams,
+            bounds[boundsIndex].signedParams,
+            bounds[boundsIndex].unsignedUpperBounds,
+            bounds[boundsIndex].unsignedLowerBounds,
+            bounds[boundsIndex].signedUpperBounds,
+            bounds[boundsIndex].signedLowerBounds
         ));
 
         // auth
@@ -96,5 +162,6 @@ contract ControllerSwapOverlay is GebAuth {
         rateSetter.updateRate(address(0x83533fdd3285f48204215E9CF38C785371258E76)); // GEB_STABILITY_FEE_TREASURY
 
         isScaled = !isScaled;
+        lastUpdateTime = now;
     }
 }
--- src/test/controller/ControllerSwapOverlay.t.sol
+++ src/test/controller/ControllerSwapOverlay.t.sol
@@ -8,12 +8,72 @@ import "../../overlays/controller/ControllerSwapOverlay.sol";
 contract RateSetterMock {
     address public pidCalculator;
 
+    constructor(address calculator) public {
+        pidCalculator = calculator;
+    }
+
     function modifyParameters(bytes32 parameter, address val) external {
         if (parameter == "pidCalculator")
             pidCalculator = val;
     }
 
     function updateRate(address) external {}
+}
+
+contract OracleRelayerMock {
+    function redemptionPrice() external returns (uint) {
+        return 3 * 10**27;
+    }
+}
+
+contract CalculatorMock {
+    function oll() external pure returns (uint) {
+        return 1;
+    }
+
+    function dos(uint) external pure returns (uint, int, int) {
+        return (
+            1646456171,
+            -878977039364788994730968,
+            -6672548645695324120975822161645
+        );
+    }
+
+    function sg() external pure returns (int) {
+        return 75000000000;
+    }
+
+    function ag() external pure returns (int) {
+        return 24000;
+    }
+
+    function pscl() external pure returns (uint) {
+        return 999999711200000000000000000;
+    }
+
+    function ips() external pure returns (uint) {
+        return 21600;
+    }
+
+    function nb() external pure returns (uint) {
+        return 1000000000000000000;
+    }
+
+    function foub() external pure returns (uint) {
+        return 1000000000000000000000000000000000000000000000;
+    }
+
+    function folb() external pure returns (int) {
+        return -999999999999999999999999999;
+    }
+
+    function lut() external pure returns (uint) {
+        return 1646456171;
+    }
+
+    function pdc() external pure returns (int) {
+        return -6672548645695324120975822161645;
+    }
 }
 
 abstract contract AuthLike {
@@ -43,33 +103,24 @@ interface Hevm {
 contract ControllerSwapOverlayTest is DSTest {
     Hevm hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
     RateSetterMock rateSetter;
+    OracleRelayerMock oracleRelayer;
     address constant pauseProxy = 0xa57A4e6170930ac547C147CdF26aE4682FA8262E;
     ControllerSwapOverlay overlay;
-    // User user;
-
-    uint256 internal constant NEGATIVE_RATE_LIMIT         = TWENTY_SEVEN_DECIMAL_NUMBER - 1;
-    uint256 internal constant TWENTY_SEVEN_DECIMAL_NUMBER = 10 ** 27;
-    uint256 internal constant EIGHTEEN_DECIMAL_NUMBER     = 10 ** 18;
-
-    int256 Kp                                 = int(EIGHTEEN_DECIMAL_NUMBER);
-    int256 Ki                                 = int(EIGHTEEN_DECIMAL_NUMBER);
-    uint256 integralPeriodSize                = 3600;
-    uint256 perSecondCumulativeLeak           = 999997208243937652252849536; // 1% per hour
-    uint256 noiseBarrier                      = EIGHTEEN_DECIMAL_NUMBER;
-    uint256 feedbackOutputUpperBound          = TWENTY_SEVEN_DECIMAL_NUMBER * EIGHTEEN_DECIMAL_NUMBER;
-    int256  feedbackOutputLowerBound          = -int(NEGATIVE_RATE_LIMIT);
+    CalculatorMock calculator;
 
     function setUp() public {
-        hevm.warp(1e6);
+        calculator = new CalculatorMock();
+        rateSetter = new RateSetterMock(address(calculator));
+        oracleRelayer = new OracleRelayerMock();
+        overlay = new ControllerSwapOverlay(
+            pauseProxy,
+            RateSetterLike(address(rateSetter)),
+            OracleRelayerLike(address(oracleRelayer)),
+            3600,
+            false
+        );
 
-        // Local
-        rateSetter = new RateSetterMock();
-        overlay = new ControllerSwapOverlay(pauseProxy, RateSetterLike(address(rateSetter)), 3600);
-
-        // Mainnet
-        // rateSetter = RateSetterMock(0x7Acfc14dBF2decD1c9213Db32AE7784626daEb48);
-        // overlay = new ControllerSwapOverlay(pauseProxy, RateSetterLike(address(rateSetter)), 3600);
-        // giveAuth(address(rateSetter), address(overlay)); // cheat
+        hevm.warp(calculator.lut());
     }
 
     function giveAuth(address _base, address target) internal {
@@ -98,7 +149,9 @@ contract ControllerSwapOverlayTest is DSTest {
     function test_setup() public {
         assertEq(address(overlay.pauseProxy()), address(pauseProxy));
         assertEq(address(overlay.rateSetter()), address(rateSetter));
-        assertEq(address(overlay.rateSetter()), address(rateSetter));
+        assertEq(address(overlay.oracleRelayer()), address(oracleRelayer));
+        assertEq(overlay.updateDelay(), 3600);
+        assertTrue(!overlay.isScaled());
     }
     function test_add_auth() public {
         overlay.addAuthorization(address(0x3));
@@ -109,56 +162,56 @@ contract ControllerSwapOverlayTest is DSTest {
         assertEq(overlay.authorizedAccounts(address(this)), 0);
     }
     function test_swap_controller() public {
-        (address calc, address calculatorOverlay) = overlay.swapCalculator(
-            Kp,
-            Ki,
-            perSecondCumulativeLeak,
-            integralPeriodSize,
-            noiseBarrier,
-            feedbackOutputUpperBound,
-            feedbackOutputLowerBound
-        );
+        (address calc, address calculatorOverlay) = overlay.swapCalculator();
 
-        PIRawPerSecondCalculator calculator = PIRawPerSecondCalculator(calc);
+        CalculatorLike newCalculator = CalculatorLike(calc);
+
+        assertEq(calculator.sg(), newCalculator.sg() * 3);
+        assertEq(calculator.ag(), newCalculator.ag() * 3);
+        assertEq(calculator.pscl(), newCalculator.pscl());
+        assertEq(calculator.ips(), newCalculator.ips());
+        assertEq(calculator.nb(), newCalculator.nb());
+        assertEq(calculator.foub(), newCalculator.foub());
+        assertEq(calculator.folb(), newCalculator.folb());
+        assertEq(calculator.lut(), newCalculator.lut());
+        assertEq(calculator.pdc(), newCalculator.pdc());
 
         assertEq(rateSetter.pidCalculator(), calc);
-        assertEq(calculator.seedProposer(), address(rateSetter));
-        assertEq(calculator.authorities(calculatorOverlay), 1);
-        assertEq(calculator.authorities(address(overlay)), 0);
+        assertEq(newCalculator.seedProposer(), address(rateSetter));
+        assertEq(newCalculator.authorities(calculatorOverlay), 1);
+        assertEq(newCalculator.authorities(address(overlay)), 0);
         assertTrue(overlay.isScaled());
+
+        hevm.warp(now + 3600);
+
+        (calc, calculatorOverlay) = overlay.swapCalculator();
+
+        newCalculator = CalculatorLike(calc);
+
+        assertEq(calculator.sg(), newCalculator.sg());
+        assertEq(calculator.ag(), newCalculator.ag());
+        assertEq(calculator.pscl(), newCalculator.pscl());
+        assertEq(calculator.ips(), newCalculator.ips());
+        assertEq(calculator.nb(), newCalculator.nb());
+        assertEq(calculator.foub(), newCalculator.foub());
+        assertEq(calculator.folb(), newCalculator.folb());
+        assertEq(calculator.lut(), newCalculator.lut());
+        assertEq(calculator.pdc(), newCalculator.pdc());
+
+        assertEq(rateSetter.pidCalculator(), calc);
+        assertEq(newCalculator.seedProposer(), address(rateSetter));
+        assertEq(newCalculator.authorities(calculatorOverlay), 1);
+        assertEq(newCalculator.authorities(address(overlay)), 0);
+        assertTrue(!overlay.isScaled());
     }
 
     function testFail_swap_controller_unauthed() public {
         overlay.removeAuthorization(address(this));
-        overlay.swapCalculator(
-            Kp,
-            Ki,
-            perSecondCumulativeLeak,
-            integralPeriodSize,
-            noiseBarrier,
-            feedbackOutputUpperBound,
-            feedbackOutputLowerBound
-        );
+        overlay.swapCalculator();
     }
     function testFail_swap_controller_too_early() public {
-        overlay.swapCalculator(
-            Kp,
-            Ki,
-            perSecondCumulativeLeak,
-            integralPeriodSize,
-            noiseBarrier,
-            feedbackOutputUpperBound,
-            feedbackOutputLowerBound
-        );
+        overlay.swapCalculator();
         hevm.warp(now+3599);
-        overlay.swapCalculator(
-            Kp,
-            Ki,
-            perSecondCumulativeLeak,
-            integralPeriodSize,
-            noiseBarrier,
-            feedbackOutputUpperBound,
-            feedbackOutputLowerBound
-        );
+        overlay.swapCalculator();
     }
 }
